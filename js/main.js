1702ed965c67110bd6a9da0550da95d3d24a5c24
Mark K. Cruz <jink144@gmail.com>
2021-04-29 11:18:31 -0400

    Update on JS adding ajax

--- js/main.js
+++ js/main.js
@@ -15,3 +15,16 @@ function handleInput(e) {
     log.textContent = ` - ${e.target.value.length} characters.`;
 }
 
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = receivePlace;
+
+function updateDB() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("GET","getuser.php?q="+str,true);
+    xmlhttp.send();
+}
